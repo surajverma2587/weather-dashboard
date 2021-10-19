120c92387898987a4372b80e0785fa597ad409df
Conor Kelly <conorjkelly96@gmail.com>
2021-10-19 20:03:05 +0100

    pushing mock data

---
+++ assets/js/index.js
@@ -0,0 +1,48 @@
+const mockData = {
+  current: {
+    name: "London",
+    temperature: 123.45,
+    wind: 111.22,
+    humidity: 33,
+    uvi: 2.5,
+    date: "(3/30/2021)",
+    iconCode: "04n",
+  },
+  forecast: [
+    {
+      date: "(3/30/2021)",
+      temperature: 123.45,
+      wind: 111.22,
+      humidity: 33,
+      iconCode: "04n",
+    },
+    {
+      date: "(3/30/2021)",
+      temperature: 123.45,
+      wind: 111.22,
+      humidity: 33,
+      iconCode: "04n",
+    },
+    {
+      date: "(3/30/2021)",
+      temperature: 123.45,
+      wind: 111.22,
+      humidity: 33,
+      iconCode: "04n",
+    },
+    {
+      date: "(3/30/2021)",
+      temperature: 123.45,
+      wind: 111.22,
+      humidity: 33,
+      iconCode: "04n",
+    },
+    {
+      date: "(3/30/2021)",
+      temperature: 123.45,
+      wind: 111.22,
+      humidity: 33,
+      iconCode: "04n",
+    },
+  ],
+};
