18b1ca2d32704e8ce0fbe3ebffbac5a4be3d74ac
Conor Kelly <conorjkelly96@gmail.com>
2021-10-19 20:21:06 +0100

    renderCurrentCard in JS added

--- assets/js/index.js
+++ assets/js/index.js
@@ -46,3 +46,32 @@ const mockData = {
     },
   ],
 };
+
+const weatherCardsContainer = $("#weather-cards-container");
+
+const renderCurrentWeatherCard = function (currentData) {
+  console.log(currentData);
+  const currentWeatherCard = `<div class="card-body bg-white border mb-2">
+    <h2 class="card-title">
+        ${currentData.name} ${currentData.date}
+        <img src="https://openweathermap.org/img/w/${currentData.iconCode}.png" />
+    </h2>
+    <p class="card-text">Temp: ${currentData.temperature}&deg;F</p>
+    <p class="card-text">Wind: ${currentData.wind} MPH</p>
+    <p class="card-text">Humidity: ${currentData.humidity}%</p>
+    <p class="card-text">
+        UV index: <span class="btn btn-primary">${currentData.uvi}</span>
+    </p>
+    </div>`;
+
+  weatherCardsContainer.append(currentWeatherCard);
+};
+
+// constructing weather cards
+const renderWeatherCards = function (weatherData) {
+  //   console.log(weatherData);
+
+  renderCurrentWeatherCard(weatherData.current);
+};
+
+renderWeatherCards(mockData);
