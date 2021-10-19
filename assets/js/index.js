8be7c154179d4466ee25bae37f26973fcfffbd9a
surajverma2587 <surajverma2587@gmail.com>
2021-10-19 21:49:25 +0100

    handle form submit

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,60 +1,46 @@
-const mockData = {
-  current: {
-    name: "London",
-    temperature: 123.45,
-    wind: 111.22,
-    humidity: 33,
-    uvi: 2.5,
-    date: "(3/30/2021)",
-    iconCode: "04n",
-  },
-  forecast: [
-    {
-      date: "(3/30/2021)",
-      temperature: 123.45,
-      wind: 111.22,
-      humidity: 33,
-      iconCode: "04n",
-    },
-    {
-      date: "(3/30/2021)",
-      temperature: 123.45,
-      wind: 111.22,
-      humidity: 33,
-      iconCode: "04n",
-    },
-    {
-      date: "(3/30/2021)",
-      temperature: 123.45,
-      wind: 111.22,
-      humidity: 33,
-      iconCode: "04n",
-    },
-    {
-      date: "(3/30/2021)",
-      temperature: 123.45,
-      wind: 111.22,
-      humidity: 33,
-      iconCode: "04n",
-    },
-    {
-      date: "(3/30/2021)",
-      temperature: 123.45,
-      wind: 111.22,
-      humidity: 33,
-      iconCode: "04n",
-    },
-  ],
-};
-
 const weatherCardsContainer = $("#weather-cards-container");
 
 const API_KEY = "393609ac7b2e5f25ccdd00e626ee13dd";
+
+const getCurrentData = function (name, forecastData) {
+  return {
+    name: name,
+    temperature: forecastData.current.temp,
+    wind: forecastData.current.wind_speed,
+    humidity: forecastData.current.humidity,
+    uvi: forecastData.current.uvi,
+    date: getFormattedDate(forecastData.current.dt),
+    iconCode: forecastData.current.weather[0].icon,
+  };
+};
+
+const getFormattedDate = function (unixTimestamp) {
+  return moment.unix(unixTimestamp).format("ddd DD/MM/YYYY");
+};
+
+const getIconCode = function () {
+  return;
+};
+
+const getForecastData = function (forecastData) {
+  const callback = function (each) {
+    return {
+      date: getFormattedDate(each.dt),
+      temperature: each.temp.max,
+      wind: each.wind_speed,
+      humidity: each.humidity,
+      iconCode: each.weather[0].icon,
+    };
+  };
+
+  return forecastData.daily.slice(1, 6).map(callback);
+};
 
 const getWeatherData = async (cityName) => {
   const currentDataUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API_KEY}`;
   const currentDataResponse = await fetch(currentDataUrl);
   const currentData = await currentDataResponse.json();
+
   const lat = currentData.coord.lat;
   const lon = currentData.coord.lon;
   const name = currentData.name;
@@ -63,53 +49,13 @@ const getWeatherData = async (cityName) => {
 
   const forecastDataResponse = await fetch(forecastDataUrl);
   const forecastData = await forecastDataResponse.json();
+
+  const current = getCurrentData(name, forecastData);
+  const forecast = getForecastData(forecastData);
+
   return {
-    current: {
-      name: name,
-      temperature: forecastData.current.temp,
-      wind: forecastData.current.wind_speed,
-      humidity: forecastData.current.humidity,
-      uvi: forecastData.current.uvi,
-      date: "(3/30/2021)",
-      iconCode: "04n",
-    },
-    forecast: [
-      {
-        date: "(3/30/2021)",
-        temperature: 123.45,
-        wind: 111.22,
-        humidity: 33,
-        iconCode: "04n",
-      },
-      {
-        date: "(3/30/2021)",
-        temperature: 123.45,
-        wind: 111.22,
-        humidity: 33,
-        iconCode: "04n",
-      },
-      {
-        date: "(3/30/2021)",
-        temperature: 123.45,
-        wind: 111.22,
-        humidity: 33,
-        iconCode: "04n",
-      },
-      {
-        date: "(3/30/2021)",
-        temperature: 123.45,
-        wind: 111.22,
-        humidity: 33,
-        iconCode: "04n",
-      },
-      {
-        date: "(3/30/2021)",
-        temperature: 123.45,
-        wind: 111.22,
-        humidity: 33,
-        iconCode: "04n",
-      },
-    ],
+    current: current,
+    forecast: forecast,
   };
 };
 
@@ -168,5 +114,20 @@ const renderWeatherCards = function (weatherData) {
   renderForecastWeatherCards(weatherData.forecast);
 };
 
-getWeatherData("london");
-renderWeatherCards(mockData);
+const handleSearch = async function (event) {
+  event.preventDefault();
+
+  const cityName = $("#city-input").val();
+
+  if (cityName) {
+    const weatherData = await getWeatherData(cityName);
+
+    weatherCardsContainer.empty();
+
+    renderWeatherCards(weatherData);
+
+    // save city to LS
+  }
+};
+
+$("#search-form").on("submit", handleSearch);
